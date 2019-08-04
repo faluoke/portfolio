62c3a1493957e7054b3d05ae09a17a49d5926a2a
Faluoke Aisikaer <15935486+faluoke@users.noreply.github.com>
2019-08-04 11:26:09 -0600

    made site mobile frendly and added lazy loading to main image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,58 @@
-import React, { Component } from "react"
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
 import indexStyle from "./index.module.css"
-import programming from "../images/programming.jpg"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Project from "../components/project"
 
-class IndexPage extends Component {
-  render() {
-    return (
-      <Layout>
-        <SEO title="Home" />
-        <div className={indexStyle.container}>
-          <div className={indexStyle.text}>
-            <p className={indexStyle.textContent}>Hi, I am Faluoke</p>
-            <p className={indexStyle.textContent}>A Developer</p>
-            <p className={indexStyle.textContent}>
-              Currently living in Salt Lake City
-            </p>
-            <button className={indexStyle.button}>
-              <a className={indexStyle.button} href="#projects">
-                Go to Projects
-              </a>
-            </button>
-          </div>
+export default props => (
+  <Layout>
+    <SEO title="Home" />
+    <div className={indexStyle.container}>
+      <Img fluid={props.data.file.childImageSharp.fluid} />
+      <div className={indexStyle.text}>
+        <p className={indexStyle.textContent}>Hi, I am Faluoke</p>
+        <p className={indexStyle.textContent}>A Developer</p>
+        <p className={indexStyle.textContent}>
+          Currently living in Salt Lake City
+        </p>
+        <button className={indexStyle.button}>
+          <a className={indexStyle.button} href="#projects">
+            Go to Projects
+          </a>
+        </button>
+      </div>
+    </div>
 
-          <img
-            src={programming}
-            alt="programming"
-            className={indexStyle.imgDisplay}
-          />
-        </div>
-        <Project />
-      </Layout>
-    )
+    <div className={indexStyle.mobile}>
+      <Img fluid={props.data.file.childImageSharp.fluid} />
+      <div className={indexStyle.text}>
+        <p className={indexStyle.textContent}>Hi, I am Faluoke</p>
+        <p className={indexStyle.textContent}>A Developer</p>
+        <p className={indexStyle.textContent}>
+          Currently living in Salt Lake City
+        </p>
+        <button className={indexStyle.button}>
+          <a className={indexStyle.button} href="#projects">
+            Go to Projects
+          </a>
+        </button>
+      </div>
+    </div>
+    <Project />
+  </Layout>
+)
+
+export const pageQuery = graphql`
+  query {
+    file(relativePath: { eq: "programming.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 5500, quality: 100, maxHeight: 2600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   }
-}
-
-export default IndexPage
+`
